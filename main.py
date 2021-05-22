00c742618525fe874823b65f68c3e51e3db231e3
Marco <marconovino@gmail.com>
2021-05-21 22:22:11 -0300

    reverted last changes

--- main.py
+++ main.py
@@ -46,6 +46,6 @@ async def graph(ctx,coin):
     figpic = discord.File("figure.png", filename="figure.png")
     embed2 = discord.Embed()
     embed2.set_image(url="attachment://figure.png")
-    await message.edit(embed=embed2)
+    await ctx.send(content = "<@"+str(ctx.author.id) + ">",file = figpic,embed=embed2)
 
 bot.run(TOKEN)
